dae6fa2a6db8f5f0d8d42bdb192cf5f887dad013
SuperHazza <27dahlbh@mbbc.qld.edu.au>
2025-08-08 15:04:18 +1000

    Kept ship on screen
    Why did the ship not fly off the screen???
    
    BECAUSE I CODED IT NOT TO

--- Objects/Ship.py
+++ Objects/Ship.py
@@ -1,4 +1,4 @@
-from GameFrame import RoomObject
+from GameFrame import RoomObject, Globals
 import pygame
 
 class Ship(RoomObject):
@@ -18,13 +18,28 @@ class Ship(RoomObject):
 
         # register events
         self.handle_key_events = True
-        
+
     def key_pressed(self, key):
         """
         Respond to keypress up and down
         """
         
         if key[pygame.K_w]:
-            self.y_speed = -10
+            self.y -= 10
         elif key[pygame.K_s]:
-            self.y_speed = 10
+            self.y += 10
+
+    def keep_in_room(self):
+        """
+        Keeps the ship inside the room
+        """
+        if self.y < 0:
+            self.y = 0
+        elif self.y + self.height> Globals.SCREEN_HEIGHT:
+            self.y = Globals.SCREEN_HEIGHT - self.height
+
+    def step(self):
+        """
+        Determine what happens to the Ship on each click of the game clock
+        """
+        self.keep_in_room()
