1b15c582a8c00cff92969c0ec9d7024e83bd9439
SuperHazza <27dahlbh@mbbc.qld.edu.au>
2025-08-15 07:56:58 +1000

    Added lives

--- Objects/Asteroid.py
+++ Objects/Asteroid.py
@@ -11,19 +11,19 @@ class Asteroid(RoomObject):
         Initialise the Asteroid object
         """
         # include attributes and methods from RoomObject
-        RoomObject.__init__(self, room, x, y)
-
+        RoomObject.__init__(self,room, x, y)
+        
         # set image
         image = self.load_image("asteroid.png")
         self.set_image(image,50,49)
-
+        
         # set travel direction
         angle = random.randint(135,225)
         self.set_direction(angle, 10)
-
+        
         # register events
         self.register_collision_object("Ship")
-
+        
     def step(self):
         """
         Determines what happens to the asteroid on each tick of the game clock
@@ -41,7 +41,7 @@ class Asteroid(RoomObject):
         elif self.y > Globals.SCREEN_HEIGHT - self.height:
             self.y = Globals.SCREEN_HEIGHT - self.height
             self.y_speed *= -1
-
+            
     def outside_of_room(self):
         """
         removes asteroid that have exited the room
@@ -49,11 +49,16 @@ class Asteroid(RoomObject):
         if self.x + self.width < 0:
             print("asteroid deleted")
             self.room.delete_object(self)
-
+            
     def handle_collision(self, other, other_type):
         """
         Handles the collision events for the Asteroid
         """
         
         if other_type == "Ship":
-            self.room.running = False
+            self.room.delete_object(self)
+            Globals.LIVES -= 1
+            if Globals.LIVES > 0:
+                self.room.lives.update_image()
+            else:
+                self.room.running = False
